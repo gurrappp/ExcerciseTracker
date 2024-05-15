1f92317c530bbbb3b2038c436dbd200f22127521
Gustav Polhem <gustavpolhem@gmail.com>
2024-05-15 22:24:58 +0200

    updates

--- ExcerciseTracker/Controllers/ExerciseController.cs
+++ ExcerciseTracker/Controllers/ExerciseController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExcerciseTracker.Models;
+using ExcerciseTracker.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,13 @@ namespace ExcerciseTracker.Controllers
 {
     internal class ExerciseController
     {
+        private readonly IExerciseRepository<Running> _exerciseRepository;
+
+        public ExerciseController(IExerciseRepository<Running> exerciseRepository)
+        {
+            _exerciseRepository = exerciseRepository;
+        }
+
+
     }
 }
